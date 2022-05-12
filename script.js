558128b56dc642255c6863ea07b86eaa9a679bbd
lucaspinheiro12 <cordeiropinheirolucas@gmail.com>
2022-05-11 23:40:29 -0300

    script

--- script.js
+++ script.js
@@ -71,7 +71,9 @@
                 navigator.clipboard.writeText(textoCriptografado);
                 encriptar.focus();
 
-
+                copiar.style.display = "none";
+                imagem.style.display = "block";
+                textoFinal.style.display = "none";
             } 
             
             function limpaTexto(){
